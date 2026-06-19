6102252bdf35a7753c4fea7eb003a62089cd5762
rdvrma <rdvrma@gmail.com>
2026-06-19 16:23:41 +0530

    analytics: add Microsoft Clarity tracking (ID: x9g2cjoeoa) to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,17 @@ export default function RootLayout({
             gtag('config', 'G-JYM8LF1BPN');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x9g2cjoeoa");
+          `}
+        </Script>
         <StyledJsxRegistry>
           <ScrollReset />
           {children}
